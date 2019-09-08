1ab5dde8edfc0f4586a58c662f719ab57dcf210b
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-09-08 23:29:13 +0200

    :tada: Load and display data from wikipedia

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,42 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+	import wtf from 'wtf_wikipedia';
+	import shuffle from 'lodash/shuffle';
+
+	let rockstars = [];
+
+	onMount(async () => {
+		const fetchJSON = async (url) => {
+			const res = await fetch(url);
+			return res.json();
+		}
+
+		rockstars = (async () => {
+			const titles = shuffle(await fetchJSON(`rockstars.json`));
+			const docs = await Promise.all(titles.map(title => wtf.fetch(title)));
+			return docs
+				.map(doc => ({
+					infobox: doc.infobox(0),
+					image: doc.images(0),
+				}))
+				.map(({ infobox, image }) => ({
+					name: infobox.get('name').text(),
+					dead: Boolean(infobox.get('death_date')),
+					image: image.thumb(),
+				}));
+		})();
+	});
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+<h1>Dead or Alive</h1>
+{#await rockstars}
+	Loading rockstars...
+{:then rockstars}
+	<ul>
+		{#each rockstars as rockstar}
+			<li><img src={rockstar.image} alt={rockstar.name}> {rockstar.name} is {rockstar.dead ? 'dead' : 'alive'}</li>
+		{/each}
+	</ul>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
