144b006fb1e077bbef608511cd10bb4bbab07a25
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-09-09 16:40:35 +0200

    :lipstick:

--- src/game/Rules.svelte
+++ src/game/Rules.svelte
@@ -11,14 +11,21 @@
 <style>
     .rules {
         text-align: center;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 </style>
 
 <div class="rules">
-    <h2>Rules</h2>
-    <p>You have 6.66 seconds to guess if a rockstar is dead or alive.</p>
-    <p>For the first 3.33 seconds you have only the rockstar's picture, then you also have her/his name.</p>
-    <p>The faster you go, the more points you earn.</p>
-    <p>The longer your good answers streak, the more points you earn.</p>
-    <p><button on:click={start}>OK, let's rock!</button></p>
+    <div>
+        <h2>Rules</h2>
+        <p>You have 6.66 seconds to guess if a rockstar is dead or alive.</p>
+        <p>For the first 3.33 seconds you have only the rockstar's picture, then you also have her/his name.</p>
+        <p>The faster you go, the more points you earn.</p>
+        <p>The longer your good answers streak, the more points you earn.</p>
+        <p><button on:click={start}>OK, let's rock!</button></p>
+    </div>
 </div>
