522686836c22003dd9c2d8b81be5591579faba23
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2019-10-08 17:31:51 +0200

    :lipstick: Button dead and alive with color

--- src/game/Rockstar.svelte
+++ src/game/Rockstar.svelte
@@ -1,8 +1,8 @@
 <script>
     import { createEventDispatcher, onDestroy } from 'svelte';
-    
+
     const dispatch = createEventDispatcher();
-    
+
     export let rockstar;
 
     let timeout;
@@ -55,15 +55,24 @@
 
     .container > button {
         width: 25%;
+        color: #ffffff;
+    }
+
+    button.dead {
+        background-color: #520000;
+    }
+
+    button.alive {
+        background-color: #273800;
     }
 </style>
 
 <div class="container">
-    <button on:click={() => answer(true)}>Dead</button>
+    <button on:click={() => answer(true)} class="dead">Dead</button>
     <div class="rockstar">
         <p><progress value={6660 - timer} max="6660"></progress></p>
         <p><img src={rockstar.image.src} alt="Rockstar picture"></p>
         <p style="visibility: {showName ? 'visible' : 'hidden'}">{rockstar.name}</p>
     </div>
-    <button on:click={() => answer(false)}>Alive</button>
+    <button on:click={() => answer(false)} class="alive">Alive</button>
 </div>
