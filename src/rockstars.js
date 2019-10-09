4318a5061ef2956ac1c91933f12f8c2f29f931bd
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2019-10-09 15:31:14 +0200

    :card_file_box: Add more IT stars

--- src/rockstars.js
+++ src/rockstars.js
@@ -35,7 +35,7 @@ export default [
         "James Hetfield",
         "Keith Richards",
         "Mark Knopfler",
-        "Mick Jagger", 
+        "Mick Jagger",
         "Ozzy Osborne",
         "Paul McCartney",
         "Pete Doherty",
@@ -66,19 +66,41 @@ export default [
 
       // Alive
         "Margaret Hamilton (software engineer)",
+        "Elizabeth_J._Feinler", //Create the ARPANET WHOIS
+        "Adele_Goldberg_(computer_scientist)", //Developping Smaltalk
+        "Marissa_Mayer", //Google et Yahoo
 
-    
       // Dead
         "Ada Lovelace",
         "Hedy Lamarr",
+        "Grace_Hopper", //First compiler COBOL
+        "Joan_Clarke", //Work on ENIGMA
 
     // Men
 
       // Alive
         "Bill Gates",
+        "Linus_Torvalds",
+        "Richard_Stallman",
+        "Gordon_Moore",
+        "Scott_Fahlman", //Emoticons
+        "Manuel_Blum", //Crypto and CAPTCHA
+        "Tim_Berners-Lee", //Word Wide Web
+        "Niklaus_Wirth", //Algol, Pascal, Oberon
+        "James_Gosling", //Java
+        "Larry_Page",
+        "Sergey_Brin",
+        "Rasmus_Lerdorf", // PHP
 
       // Dead
         "Charles Babbage",
         "John McCarthy (computer scientist)",
         "Steve Jobs",
+        "Edsger_W._Dijkstra",
+        "George_Boole", //Boolean algebra
+        "Alan_Turing",
+        "Douglas_Engelbart", //Computer mouse
+        "Dennis_Ritchie", // Language C
+        "Paul_Allen", //Microsoft
+        "Adam_Osborne", //First portable computer
 ]
