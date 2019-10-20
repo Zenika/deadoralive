a428373de058c67235bbe176ae8c5563a06a9bd4
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-10-20 23:17:27 +0200

    :construction: Add router

--- src/App.svelte
+++ src/App.svelte
@@ -1,71 +1,15 @@
 <script>
-	import { onMount } from 'svelte'
-	import wtf from 'wtf_wikipedia'
-	import Header from './Header.svelte'
-	import Landing from './Landing.svelte'
-	import Game from './game'
-	import titles from './rockstars'
-	import {connected} from './firebase.js'
+  import { Router, Route } from "svelte-routing";
+  import Header from './Header.svelte'
+  import Index from "./Index.svelte";
+  import Scores from "./Scores.svelte";
 
-	let rockstars = []
-	let game = null
-	let player = null
-
-	onMount(async () => {
-		const prefetchImage = url => new Promise((resolve, reject) => {
-			const image = new Image()
-			image.onload = () => resolve(image)
-			image.onerror = () => reject(Error('Error while prefetching image'))
-			image.src = url
-		})
-
-		const fetchRockstar = async (title) => {
-			const doc = await wtf.fetch(title)
-			if (!doc) throw Error(`Unknown wikipedia page "${title}"`)
-
-			const infobox = doc.infobox(0)
-
-			const name = infobox.get('name')
-
-			const imageObj = doc.images(0)
-			if (!imageObj) throw Error(`"${title}" wikipedia page has no image`)
-
-			const image = await prefetchImage(imageObj.thumb())
-
-			return {
-				name: name ? name.text() : title,
-				dead: Boolean(infobox.get('death_date')),
-				image,
-			}
-		}
-
-		rockstars = Promise.all(titles.map(fetchRockstar))
-	})
-
-	const clearGame = ({ detail: { keepPlayer } }) => {
-		player = keepPlayer ? game.player : null;
-		game = null;
-	}
+  export let url = "";
 </script>
 
 <Header />
 
-<main>
-	{#await connected}
-		<p class="loader">Connecting...</p>
-	{:then connected}
-		{#await rockstars}
-			<p class="loader">Loading rockstars...</p>
-		{:then rockstars}
-			{#if game}
-				<Game {rockstars} {game} on:clearGame={clearGame} />
-			{:else}
-				<Landing on:newGame={event => game = event.detail} {player} />
-			{/if}
-		{:catch error}
-			<p class="error">{error.message}</p>
-		{/await}
-	{:catch error}
-		<p class="error">{error.message}</p>
-	{/await}
-</main>
+<Router url="{url}">
+    <Route path="/" component={Index} />
+	<Route path="/scores" component={Scores} />
+</Router>
